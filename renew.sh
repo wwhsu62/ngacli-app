93a68c096ab28bf348725c6da8a5149ba9c05c59
wwhsu62 <42960370+wwhsu62@users.noreply.github.com>
2018-09-07 15:15:33 +0800

    Delete renew.sh

--- renew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop ngacli-app
-docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm
-docker rmi $(docker images | grep -v 'ubuntu\|my-image' | awk {'print $3'})
-docker-compose up -d
